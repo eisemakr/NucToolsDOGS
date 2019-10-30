227a0ecfed2de8cec3a25d1fd8cee2953b909d48
Krautter <eike.krautter@igb.fraunhofer.de>
2019-10-30 14:24:52 +0100

    Testing phase... ensuring everything runs as it should. In Excel for small sample no problem.

--- DOGdirection.R
+++ DOGdirection.R
@@ -3,13 +3,37 @@
 #added to the second file and a new output is produced. 
 
 if (!require("pacman")) install.packages("https://cran.r-project.org/src/contrib/pacman_0.4.6.tar.gz",repos=NULL, method="libcurl")
-pacman::p_load( biomaRt, dplyr )
+pacman::p_load( dplyr )
 
 args <- commandArgs(TRUE)
-p2data <- as.character(args[1])   # path to DOG file
-orga <- as.character(args[2])   # path to Gene file
+p2DOG <- as.character(args[1])   # path to DOG file
+p2gene <- as.character(args[2])   # path to Gene file
+
+#################################################################################
+# For development
+p2DOG <- 'W:/db05/Eike/mirsi/Analysis/stabfuz/Control/dev/con1_con2/TestDOGS.bed' 
+p2gene <- 'W:/db05/Eike/mirsi/Analysis/stabfuz/Control/dev/con1_con2/DOGS2dirgene1000.bed'
+#################################################################################
 
 if(is.na(args[1])|is.na(args[2])){
   print('please specify both arguments!')
   quit()
 }
+
+DOGS <- read.table(p2DOG, fill = T, header = F)
+GENES <- read.table(p2gene, fill = T, header = F)
+
+#################################################################################
+#Extract the DOG identifiers from the gene file
+#################################################################################
+
+DOGSlist <- as.character(unique(GENES$V9))
+
+#################################################################################
+#Subset the DOG file by only the DOGS matching a gene 
+#################################################################################
+
+DOGSwGENES <- DOGS[which(DOGS$V4 %in% DOGSlist),]
+
+View(merge(GENES,DOGSwGENES, by.x = 'V9', by.y = 'V4'))
+
